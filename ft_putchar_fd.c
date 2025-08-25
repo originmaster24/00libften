cb0677eed9db6d281751be284d72824975913a55
originmaster24 <originmaster24@gmail.com>
2025-08-25 16:52:56 +0200

    ft_putchar_fd ok ft_strmapi ok some work on otw

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,18 +6,30 @@
 /*   By: zzhu <zzhu@student.42firenze.it>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 10:03:25 by zzhu              #+#    #+#             */
-/*   Updated: 2025/08/22 10:03:56 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/08/25 16:31:11 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putchar_fd(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
-	
+	write(fd, &c, 1);
 }
 
-int main(void)
-{
-	
-}
+// int main(void)
+// {
+// ft_putchar_fd('h', 1);
+// ft_putchar_fd('e', 1);
+// ft_putchar_fd('l', 1);
+// ft_putchar_fd('l', 1);
+// ft_putchar_fd('o', 1);
+// ft_putchar_fd(' ', 1);
+// ft_putchar_fd('w', 1);
+// ft_putchar_fd('o', 1);
+// ft_putchar_fd('r', 1);
+// ft_putchar_fd('l', 1);
+// ft_putchar_fd('d', 1);
+// ft_putchar_fd('!', 1);
+// ft_putchar_fd('\n', 1);
+// }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,8 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zzhu <zzhu@student.42firenze.it>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 16:51:03 by zzhu              #+#    #+#             */
+/*   Updated: 2025/08/25 16:51:43 by zzhu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*ptr;
+	int		i;
+	int		len;
 
+	ptr = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!len)
+		return (NULL);
+	i = -1;
+	ft_memset(ptr, '\0', ft_strlen(s) + 1);
+	while (++i < ft_strlen(s))
+	{
+		ptr[i] = f(i, s[i]);
+	}
+	return (ptr);
 }
 
 // int main(void)
