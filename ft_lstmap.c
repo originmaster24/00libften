629e3268fdbe2b3b4ea9c48c436d076f29835983
originmaster24 <originmaster24@gmail.com>
2025-09-20 11:44:24 +0200

    lstmap ok, unit 1 complete

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: zzhu <zzhu@student.42firenze.it>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 21:00:49 by zzhu              #+#    #+#             */
-/*   Updated: 2025/09/19 22:16:18 by zzhu             ###   ########.fr       */
+/*   Updated: 2025/09/20 11:42:55 by zzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*resptr_start;
 	t_list	*tmpnode;
 
-	// resptr = (t_list *)malloc(sizeof(t_list) * ft_lstsize(lst));
-	// if (resptr == NULL)
-	// 	return ;
-	//make a copy of the original list
 	resptr = NULL;
 	while (lst != NULL)
 	{
@@ -29,44 +25,50 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		if (tmpnode == NULL)
 		{
 			ft_lstclear(&resptr, del);
-			return NULL;
+			return (NULL);
 		}
-		if (resptr == NULL)
-			resptr = tmpnode;
 		ft_lstadd_back(&resptr, tmpnode);
 		lst = lst -> next;
 	}
 	resptr_start = resptr;
 	while (resptr != NULL)
 	{
-		f(resptr);
+		if (!f(resptr -> content))
+			ft_lstclear(&resptr_start, del);
 		resptr = resptr -> next;
 	}
 	return (resptr_start);
 }
 
-void f (void *arg)
-{
-	printf("%s\n", (char *)arg); 
-}
+// void f(void *arg)
+// {
+// 	printf("%s\n", (char *)arg); 
+// }
+// void *ff(void *arg)
+// {
+// 	printf("%s\n", (char *)arg); 
+// 	return (arg);
+// }
 
-int main(void)
-{
-	int listn = 5;
-	t_list *liststart;
-	t_list *tmpnode;
-	char value[] = "0";
+// int main(void)
+// {
+// 	int listn = 5;
+// 	t_list *liststart;
+// 	t_list *listcpy;
 
-	liststart = NULL;
-	for(int i = 1; i <= listn; i++)
-	{
-		value[0] = i + '0';
-		tmpnode = ft_lstnew(ft_strdup(value));
-		if (tmpnode == NULL)
-			printf("error test new node fail allocation\n");
-		ft_lstadd_back(&liststart, tmpnode);
-	}
-	ft_lstiter(liststart, f);
-	
-	
-}
+// 	t_list *tmpnode;
+// 	char value[] = "0";
+
+// 	liststart = NULL;
+// 	for(int i = 1; i <= listn; i++)
+// 	{
+// 		value[0] = i + '0';
+// 		tmpnode = ft_lstnew(ft_strdup(value));
+// 		if (tmpnode == NULL)
+// 			printf("error test new node fail allocation\n");
+// 		ft_lstadd_back(&liststart, tmpnode);
+// 	}
+// 	ft_lstiter(liststart, f);
+// 	listcpy = ft_lstmap(liststart, ff, free);
+// 	ft_lstiter(listcpy, f);
+// }
